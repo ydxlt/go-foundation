db1b5d64a0210e6c608d1e0b97f706d018680254
luotong <luotong@codeffect.top>
2025-10-21 11:21:43 +0800

    update

--- sign/sign.go
+++ sign/sign.go
@@ -17,9 +17,6 @@ type Options struct {
 }
 
 func (o *Options) validate() {
-	if o.Sign == "" {
-		panic(errors.New("sign is required"))
-	}
 	if o.AccessKey == "" {
 		panic(errors.New("access_key is required"))
 	}
@@ -43,6 +40,9 @@ func CheckSign(ctx *gin.Context) {
 		}
 	}
 	sign := ctx.GetHeader(dto.HeaderAppSign)
+	if sign == "" {
+		sign = ctx.GetHeader("sign") // 兼容老版本
+	}
 	if sign != SharedOptions.Sign {
 		log.Errorf("Error sign： %s\n", sign)
 		ctx.AbortWithStatusJSON(http.StatusUnauthorized, dto.UnauthorizedError())
@@ -54,9 +54,12 @@ func CheckSign(ctx *gin.Context) {
 func CheckAccessKey(ctx *gin.Context) {
 	requiredConfigs()
 	// access_key获取不到?
-	accessKey := ctx.GetHeader("access_key")
+	accessKey := ctx.GetHeader(dto.HeaderAccessKey)
+	if accessKey == "" {
+		accessKey = ctx.GetHeader("access_key") // 兼容老版本
+	}
 	if accessKey != SharedOptions.AccessKey {
-		log.Errorf("Error accesskey %s\n", accessKey)
+		log.Errorf("Error %s %s\n", dto.HeaderAccessKey, accessKey)
 		ctx.AbortWithStatusJSON(http.StatusUnauthorized, dto.UnauthorizedError())
 		return
 	}
